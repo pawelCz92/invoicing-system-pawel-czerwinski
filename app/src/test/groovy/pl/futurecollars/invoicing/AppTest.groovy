42d4e81bf167b40fd83d298c135a6ea49eb51158
pawelCz92 <pawel.czerwinski992@gmail.com>
2021-06-08 21:24:40 +0200

    Jacoco configure done + master.yml

--- app/src/test/groovy/pl/futurecollars/invoicing/AppTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/AppTest.groovy
@@ -14,4 +14,11 @@ class AppTest extends Specification {
         then:
         result != null
     }
+
+    def "dummy test to cover main"(){
+        setup:
+        def app = new App()
+
+        and: app.main()
+    }
 }
