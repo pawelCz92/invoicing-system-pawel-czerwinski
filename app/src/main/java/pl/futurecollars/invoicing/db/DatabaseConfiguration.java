f556738851523cc56529089239eccc29b16a180a
pawelCz92 <pawel.czerwinski992@gmail.com>
2021-07-04 14:52:37 +0200

    In memory or in file base depends on property in config file

--- app/src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
@@ -1,11 +1,14 @@
 package pl.futurecollars.invoicing.db;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.futurecollars.invoicing.service.IdProvider;
 import pl.futurecollars.invoicing.service.JsonService;
 import pl.futurecollars.invoicing.service.file.FileService;
 
+@Slf4j
 @Configuration
 public class DatabaseConfiguration {
 
@@ -17,9 +20,18 @@ public class DatabaseConfiguration {
         return new IdProvider(DB_ID_FILE_NAME_PATH);
     }
 
+    @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "file")
     @Bean
     FileBasedDatabase fileBasedDatabase(JsonService jsonService, IdProvider idProvider) {
         FileService fileServiceForData = new FileService(DB_DATA_FILE_NAME_PATH);
+        log.info("File based database is in use");
         return new FileBasedDatabase(fileServiceForData, idProvider, jsonService);
+    }
+
+    @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "memory")
+    @Bean
+    InMemoryDataBase inMemoryDataBase(){
+        log.info("In memory based database is in use");
+        return new InMemoryDataBase();
     }
 }
