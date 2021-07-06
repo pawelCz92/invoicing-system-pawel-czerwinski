3d5003700c9be6c092715618fe2ec6ebb90c0241
pawelCz92 <pawel.czerwinski992@gmail.com>
2021-07-06 17:09:50 +0200

    made corrections

--- app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -2,7 +2,6 @@ package pl.futurecollars.invoicing.controller;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
