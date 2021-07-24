eeca8928480f542494f204e810fa32d561061515
pawelCz92 <pawel.czerwinski992@gmail.com>
2021-07-24 19:53:38 +0200

    Made correction to V8 migration and created V9 migration for insert data to tables

--- app/src/main/resources/db/migration/V8__Connect_invoices_with_invoice_entries.sql
+++ app/src/main/resources/db/migration/V8__Connect_invoices_with_invoice_entries.sql
@@ -11,7 +11,7 @@ ALTER TABLE public.invoices_invoice_entries
 
 ALTER TABLE public.invoices_invoice_entries
     ADD CONSTRAINT invoice_entry_id FOREIGN KEY (invoice_entry_id)
-        REFERENCES public.invoices (id);
+        REFERENCES public.invoice_entries (id);
 
 ALTER TABLE public.invoices_invoice_entries
     OWNER TO postgres;
