48622b562ff4f1eff02e55294f74b1aae47590c4
pawelCz92 <pawel.czerwinski992@gmail.com>
2021-09-01 16:57:52 +0200

    first part of tests

--- angular-frontend/invoicing-app/src/app/app.component.spec.ts
+++ angular-frontend/invoicing-app/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -21,4 +21,6 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('invoicing-app');
   });
+
+
 });
--- angular-frontend/invoicing-app/src/app/company/company/company.component.spec.ts
+++ angular-frontend/invoicing-app/src/app/company/company/company.component.spec.ts
@@ -1,4 +1,4 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {CompanyComponent} from './company.component';
 import {FormsModule} from '@angular/forms';
 import {CompanyService} from './companyService';
@@ -7,7 +7,12 @@ import {Observable, of} from 'rxjs';
 
 
 describe('CompanyComponent', () => {
+
+  let fixture: ComponentFixture<CompanyComponent>;
+  let component: CompanyComponent;
+
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
       providers: [
         {provide: CompanyService, useClass: MockCompanyService}
@@ -17,6 +22,12 @@ describe('CompanyComponent', () => {
       ],
       imports: [FormsModule]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(CompanyComponent);
+    component = fixture.componentInstance;
+
+    component.ngOnInit();
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
@@ -25,42 +36,70 @@ describe('CompanyComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'invoicing-app'`, () => {
-  //   const fixture = TestBed.createComponent(CompanyComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('invoicing-app');
-  // });
+  it(`should display list of companies`, () => {
+      const fixture = TestBed.createComponent(CompanyComponent);
+      const app = fixture.componentInstance;
+      app.ngOnInit();
+      fixture.detectChanges();
 
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(CompanyComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement;
-//     expect(compiled.querySelector('h2.titleHeader').textContent).toContain('Company');
-//   });
-// });
+      expect(fixture.nativeElement.innerText).toContain('111-111-11-11	ul. First 1	First Ltd.	111.11	1111.11');
+      expect(fixture.nativeElement.innerText).toContain('222-222-22-22	ul. Second 2	Second Ltd.	222.22	2222.22');
+
+      expect(app.companies.length).toBe(2);
+      expect(app.companies).toBe(MockCompanyService.companies);
+    }
+  );
+
+  it(`newly added company is added to the list`, () => {
+    const taxIdInput: HTMLInputElement = fixture.nativeElement.querySelector('input[name=taxIdentificationNumber]');
+    taxIdInput.value = '333-333-33-33';
+    taxIdInput.dispatchEvent(new Event('input'));
+
+    const nameInput: HTMLInputElement = fixture.nativeElement.querySelector('input[name=name]');
+    nameInput.value = 'Third Ltd.';
+    nameInput.dispatchEvent(new Event('input'));
+
+    const addressInput: HTMLInputElement = fixture.nativeElement.querySelector('input[name=address]');
+    addressInput.value = 'ul. Third 3';
+    addressInput.dispatchEvent(new Event('input'));
+
+    const addInvoiceBtn: HTMLElement = fixture.nativeElement.querySelector('#addCompanyBtn');
+    addInvoiceBtn.click();
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.innerText).toContain('333-333-33-33	ul. Third 3	Third Ltd.	0	0');
+  });
 
   class MockCompanyService {
-    companies: Company[] = [
+
+    static companies: Company[] = [
+
       new Company(
         1,
-        '22-143-12-44',
-        'ul. Dluga 1',
-        'Sony.',
-        12.121,
-        22122.12
+        '111-111-11-11',
+        'ul. First 1',
+        'First Ltd.',
+        1111.11,
+        111.11
       ),
       new Company(
         2,
-        '244-123-89-21',
-        'ul. Krótka  2',
-        'jbl',
-        12122.22,
-        333.22
+        '222-222-22-22',
+        'ul. Second 2',
+        'Second Ltd.',
+        2222.22,
+        222.22
       )
     ];
 
     getCompanies(): Observable<Company[]> {
-      return of(this.companies);
+      return of(MockCompanyService.companies);
+    }
+
+    addCompany(company: Company): Observable<Company> {
+      MockCompanyService.companies.push(company);
+      return of();
     }
   }
 });
