3397e7e7179b93dbfba6c59b053821f7ae64dcc3
pawelCz92 <pawel.czerwinski992@gmail.com>
2021-09-02 16:20:11 +0200

    task done

--- angular-frontend/invoicing-app/e2e/src/company.e2e-spec.ts
+++ angular-frontend/invoicing-app/e2e/src/company.e2e-spec.ts
@@ -1,4 +1,5 @@
 import {CompanyPage} from './company.po';
+import {CompanyRow} from './companyRow';
 
 describe('Company page E2E test', () => {
   let page: CompanyPage;
@@ -8,6 +9,13 @@ describe('Company page E2E test', () => {
 
     await page.navigateTo();
 
+    await page.companyRows()
+      .each(async (row) => {
+        const companyRow = new CompanyRow(row);
+        await companyRow.deleteBtn().click();
+      });
+
+    expect(await page.companyRows()).toEqual([]);
   });
 
   it('should display correct values for table headers', async () => {
@@ -16,5 +24,41 @@ describe('Company page E2E test', () => {
     expect(await page.addressHeaderValue()).toEqual('Address');
     expect(await page.pensionInsuranceHeaderValue()).toEqual('Pension insurance');
     expect(await page.healthInsuranceHeaderValue()).toEqual('Health insurance');
+  });
+
+  it('can add company', async () => {
+    await page.addNewCompany('123', '123 Inc.', '123 Wall Street', 1234, 123);
+
+    await page.companyRows().then(async rows => {
+      expect(rows.length).toEqual(1);
+      await new CompanyRow(rows[0]).assertRowValues('123', '123 Inc.', '123 Wall Street', '123', '1234');
+    });
+  });
+
+  it('can delete company', async () => {
+    await page.addNewCompany('123', '123 Inc.', '123 Wall Street', 123, 1234);
+    await page.addNewCompany('456', '456 Inc.', '456 Wall Street', 567, 5678);
+
+    await page.companyRows().then(async rowsBeforeDelete => {
+      expect(rowsBeforeDelete.length).toEqual(2);
+      await new CompanyRow(rowsBeforeDelete[0]).deleteBtn().click();
+
+      await page.companyRows().then(async rowsAfterDelete => {
+        expect(rowsAfterDelete.length).toEqual(1);
+        await new CompanyRow(rowsAfterDelete[0]).assertRowValues('456', '456 Inc.',
+          '456 Wall Street', '5678', '567');
+
+      });
+    });
+  });
+
+  it('can update company', async () => {
+    await page.addNewCompany('123', '123 Inc.', '123 Wall Street', 1234, 123);
+
+    await page.companyRows().then(async rows => {
+      const companyRow = new CompanyRow(rows[0]);
+      await companyRow.updateCompany('456', '456 Inc.', '456 Wall Street', 5678, 567);
+      await companyRow.assertRowValues('456', '456 Inc.', '456 Wall Street', '5678', '567');
+    });
   });
 });
--- angular-frontend/invoicing-app/e2e/src/company.po.ts
+++ angular-frontend/invoicing-app/e2e/src/company.po.ts
@@ -1,4 +1,4 @@
-import {browser, by, element} from 'protractor';
+import {browser, by, element, ElementArrayFinder, ElementFinder, WebElement} from 'protractor';
 
 export class CompanyPage {
   async navigateTo(): Promise<unknown> {
@@ -23,5 +23,51 @@ export class CompanyPage {
 
   async healthInsuranceHeaderValue(): Promise<string> {
     return element(by.id('healthInsuranceHeader')).getText();
+  }
+
+  companyRows(): ElementArrayFinder {
+    return element.all(by.css('.companyRow'));
+  }
+
+  anyCompanyRow(): ElementFinder {
+    return element(by.css('.companyRow'));
+  }
+
+  deleteBtn(row: ElementFinder): WebElement {
+    return row.element(by.css('.btn-danger'));
+  }
+
+  async addNewCompany(taxId: string, name: string, address: string, healthInsurance: number, pensionInsurance: number) {
+    await this.taxIdInput().sendKeys(taxId);
+    await this.nameInput().sendKeys(name);
+    await this.addressInput().sendKeys(address);
+
+    await this.healthInsuranceInput().clear();
+    await this.healthInsuranceInput().sendKeys(healthInsurance);
+
+    await this.pensionInsuranceInput().clear();
+    await this.pensionInsuranceInput().sendKeys(pensionInsurance);
+
+    await element(by.id('addCompanyBtn')).click();
+  }
+
+  private addressInput() {
+    return element(by.css('input[name=address]'));
+  }
+
+  private nameInput() {
+    return element(by.css('input[name=name]'));
+  }
+
+  private taxIdInput() {
+    return element(by.css('input[name=taxIdentificationNumber]'));
+  }
+
+  private healthInsuranceInput() {
+    return element(by.css('input[name=healthInsurance]'));
+  }
+
+  private pensionInsuranceInput() {
+    return element(by.css('input[name=pensionInsurance]'));
   }
 }
